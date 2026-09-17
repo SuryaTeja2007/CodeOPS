4711681878a63772c1ad812e5cf841c91e985258
SuryaTeja <stsurya20@gmail.com>
2026-09-17 12:02:30 +0530

    Add leaderboard model

---
+++ server/src/models/Leaderboard.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const leaderboardSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    nickname: { type: String, default: "", trim: true },
+    division: { type: String, default: "", trim: true },
+    xp: { type: Number, default: 0, min: 0 },
+    missions: { type: Number, default: 0, min: 0 },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Leaderboard", leaderboardSchema);
