1b1dd34f87d6b043351260d0ecaa50732940c924
SuryaTeja <stsurya20@gmail.com>
2026-09-18 12:48:25 +0530

    Fix asset URL resolver

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,5 @@
 const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-export const ASSET_BASE = API_BASE.replace(/\\/api\\/?$/, "");
+export const ASSET_BASE = API_BASE.replace(/\/api\/?$/, "");
 export const resolveAssetUrl = (url) => url && url.startsWith("/") ? `${ASSET_BASE}${url}` : url;
 
 async function request(path, options = {}) {
